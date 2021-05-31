ec86163273fdc7e41ad4511b92afb5eed9c59d99
计算机1801-包家华-20184378 <52906012+Samasara1@users.noreply.github.com>
2021-05-31 23:45:41 +0800

    更新头文件定义

--- file-struct.h
+++ file-struct.h
@@ -4,14 +4,22 @@
 #include <vector>
 #include <string>
 #include <map>
+#include "configure.h"
 
 using namespace std;
 
 class SFD_ITEM
 {
+public:
+    string getFile_name() const;
+    void setFile_name(const string &value);
+
+    int getID() const;
+    void setID(int value);
+
 private:
-   string file_name;
-   unsigned int ID;
+    string file_name;
+    int ID;
 };
 
 // 文件目录结构SFD
@@ -21,12 +29,15 @@ private:
     vector<SFD_ITEM> SFD_list;
 public:
     SFD();
+    // 在SFD中通过文件名寻找索引序号 返回-1表示没有
+    int findSfd_item(string file_name);
 };
 
 enum file_type
 {
     directory,common,special
 };
+
 
 class BFD_ITEM_DISK
 {
@@ -70,26 +81,87 @@ public:
 };
 
 // TODO: 超级块SUPER_BLOCK
-class SUPER_BLOCK{
+class SUPER_BLOCK
+{
+public:
+    //TODO: 文件恢复 避免开机重启
+    static bool file_recovery()
+    {
+        return false;
+    }
+
+    // 重构默认构造
+    SUPER_BLOCK()
+    {
+        this->super_block_num = SUPERFREEBLOCK;
+        this->disk_num_list.resize(50);
+
+    }
+    int getSuper_block_size() const;
+    void setSuper_block_size(int value);
+
+    SUPER_BLOCK *getNext_super_block() const;
+    void setNext_super_block(SUPER_BLOCK *value);
+
+    int getSuper_block_num() const;
+    void setSuper_block_num(int value);
+
+    vector<int> getDisk_num_list() const;
+    void setDisk_num_list(const vector<int> &value);
+
+private:
+    // 存放空块号，默认为50个
+    int super_block_size;
+    // 存放盘块的序号，相当于索引
+    vector<int> disk_num_list;
+    // 存放下一个超级块的指针
+    SUPER_BLOCK* next_super_block;
+    // 存放超级块序号（或许用不到）
+    int super_block_num;
 
 };
 
 // TODO: 磁盘块
-class DISK_BLOCK{
+class DISK_BLOCK
+{
+public:
+    int getBlock_num() const;
+    void setBlock_num(int value);
 
-};
+    bool getIs_full() const;
+    void setIs_full(bool value);
 
-// TODO: DISK
-class DISK{
+    string getFile_name() const;
+    void setFile_name(const string &value);
+
 private:
-    SUPER_BLOCK Super_block;
-    BFD_ITEM_DISK d_inode;
-    vector<DISK_BLOCK> d_block;
-
+    // 在磁盘中的序号即为索引
+    int block_num;
+    // bool判断是否为空 true即为非空
+    bool is_full;
+    // 对应的文件名
+    string file_name;
 };
 
 // 用户表USER
-class USER{
+class USER
+{
+public:
+    unsigned int getId() const;
+    void setId(unsigned int value);
+
+    string getUsername() const;
+    void setUsername(const string &value);
+
+    string getUserpwd() const;
+    void setUserpwd(const string &value);
+
+    int getUsergrp() const;
+    void setUsergrp(int value);
+
+    bool getIsManager() const;
+    void setIsManager(bool value);
+
 private:
     unsigned int id;    //用户id号
     string username;
@@ -98,5 +170,221 @@ private:
     bool isManager;     //管理员
 };
 
+// TODO: DISK
+class DISK
+{
+public:
+    SUPER_BLOCK getSuper_block() const;
+    void setSuper_block(const SUPER_BLOCK &value);
+
+    BFD_DISK getD_inodes() const;
+    void setD_inodes(const BFD_DISK &value);
+
+    vector<DISK_BLOCK> getD_block() const;
+    void setD_block(const vector<DISK_BLOCK> &value);
+
+    USER getUser_table() const;
+    void setUser_table(const USER &value);
+
+private:
+    // 成组链接的首个超级块
+    SUPER_BLOCK Super_block;
+    // i结点信息
+    BFD_DISK d_inodes;
+    // 块信息
+    vector<DISK_BLOCK> d_block;
+    // 用户表
+    USER user_table;
+};
+
+
+// 方法实现
+int SUPER_BLOCK::getSuper_block_size() const
+{
+return super_block_size;
+}
+
+void SUPER_BLOCK::setSuper_block_size(int value)
+{
+super_block_size = value;
+}
+
+SUPER_BLOCK *SUPER_BLOCK::getNext_super_block() const
+{
+return next_super_block;
+}
+
+void SUPER_BLOCK::setNext_super_block(SUPER_BLOCK *value)
+{
+next_super_block = value;
+}
+
+int SUPER_BLOCK::getSuper_block_num() const
+{
+return super_block_num;
+}
+
+void SUPER_BLOCK::setSuper_block_num(int value)
+{
+super_block_num = value;
+}
+
+vector<int> SUPER_BLOCK::getDisk_num_list() const
+{
+return disk_num_list;
+}
+
+void SUPER_BLOCK::setDisk_num_list(const vector<int> &value)
+{
+disk_num_list = value;
+}
+
+string SFD_ITEM::getFile_name() const
+{
+return file_name;
+}
+
+void SFD_ITEM::setFile_name(const string &value)
+{
+file_name = value;
+}
+
+int SFD_ITEM::getID() const
+{
+return ID;
+}
+
+void SFD_ITEM::setID(int value)
+{
+ID = value;
+}
+
+int SFD::findSfd_item(string file_name)
+{
+    for(auto i:SFD_list)
+    {
+        if(i.getFile_name() == file_name)
+            return i.getID();
+    }
+    return -1;
+}
+int DISK_BLOCK::getBlock_num() const
+{
+return block_num;
+}
+
+void DISK_BLOCK::setBlock_num(int value)
+{
+block_num = value;
+}
+
+bool DISK_BLOCK::getIs_full() const
+{
+return is_full;
+}
+
+void DISK_BLOCK::setIs_full(bool value)
+{
+is_full = value;
+}
+
+string DISK_BLOCK::getFile_name() const
+{
+return file_name;
+}
+
+void DISK_BLOCK::setFile_name(const string &value)
+{
+file_name = value;
+}
+
+unsigned int USER::getId() const
+{
+return id;
+}
+
+void USER::setId(unsigned int value)
+{
+id = value;
+}
+
+string USER::getUsername() const
+{
+return username;
+}
+
+void USER::setUsername(const string &value)
+{
+username = value;
+}
+
+string USER::getUserpwd() const
+{
+return userpwd;
+}
+
+void USER::setUserpwd(const string &value)
+{
+userpwd = value;
+}
+
+int USER::getUsergrp() const
+{
+return usergrp;
+}
+
+void USER::setUsergrp(int value)
+{
+usergrp = value;
+}
+
+bool USER::getIsManager() const
+{
+return isManager;
+}
+
+void USER::setIsManager(bool value)
+{
+isManager = value;
+}
 
 #endif // FILESTRUCT_H
+SUPER_BLOCK DISK::getSuper_block() const
+{
+return Super_block;
+}
+
+void DISK::setSuper_block(const SUPER_BLOCK &value)
+{
+Super_block = value;
+}
+
+BFD_DISK DISK::getD_inodes() const
+{
+return d_inodes;
+}
+
+void DISK::setD_inodes(const BFD_DISK &value)
+{
+d_inodes = value;
+}
+
+vector<DISK_BLOCK> DISK::getD_block() const
+{
+return d_block;
+}
+
+void DISK::setD_block(const vector<DISK_BLOCK> &value)
+{
+d_block = value;
+}
+
+USER DISK::getUser_table() const
+{
+return user_table;
+}
+
+void DISK::setUser_table(const USER &value)
+{
+user_table = value;
+}
