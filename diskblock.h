ba49d4ba134957c44faa5d465b644a0b6f9fc82d
Reilkay <zrztt@outlook.com>
2021-06-01 15:50:11 +0800

    feat(diskblock.h): 完善磁盘块结构

--- diskblock.h
+++ diskblock.h
@@ -2,7 +2,12 @@
 #define DISKBLOCK_H
 
 #include <string>
+#include "configure.h"
 using namespace std;
+
+enum diskblock_type {
+    content, index
+};
 
 // TODO: 磁盘块
 class DISK_BLOCK
@@ -15,15 +20,25 @@ public:
     void setIs_full(bool value);
 
     string getFile_name() const;
-    void setFile_name(const string &value);
+    void setFile_name(const string & value);
 
 private:
     // 在磁盘中的序号即为索引
     int block_num;
     // bool判断是否为空 true即为非空
     bool is_full;
-    // 对应的文件名
+    // 对应的文件名(???)
     string file_name;
+    // 磁盘块类型
+    diskblock_type block_type;
+    // 文件内容
+    char data[BLOCKSIZE];
+    // 占用空间
+    int space_used;
+    // 索引
+    int index[BLOCKSIZE / sizeof (int)];
+    // 索引个数
+    int index_num;
 };
 
 #endif // DISKBLOCK_H
