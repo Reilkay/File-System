bac10593d495fdbd2d000972f8d9e3e99810d130
计算机1801-包家华-20184378 <52906012+Samasara1@users.noreply.github.com>
2021-06-02 09:51:13 +0800

    增加d_inode和m_inode接口和实现

---
+++ dinode.cpp
@@ -0,0 +1,113 @@
+#include "dinode.h"
+
+BFD_ITEM_DISK::BFD_ITEM_DISK(unsigned int dinode_ID,     // i结点的id
+                             unsigned int master_ID,     // 文件拥有者ID
+                             file_type f_type,           // 文件类型
+                             vector<char> auth,          // 权限（三位十进制数表示）
+                             unsigned long long f_size,  // 文件大小
+                             unsigned int f_addr,        // 文件地址（物理地址/下级目录ID）
+                             unsigned int f_link_num,    // 文件链接计数
+                             time_t f_creat_time,        // 文件创建时间
+                             time_t f_change_time)      // 文件存取时间
+{
+    this->auth.resize(3);
+    this->setDinode_ID(dinode_ID);
+    this->setMaster_ID(master_ID);
+    this->setF_type(f_type);
+    this->setAuth(auth);
+    this->setF_size(f_size);
+    this->setF_addr(f_addr);
+    this->setF_link_num(f_link_num);
+    this->setF_creat_time(f_creat_time);
+    this->setF_change_time(f_change_time);
+}
+
+unsigned int BFD_ITEM_DISK::getMaster_ID() const
+{
+    return master_ID;
+}
+
+void BFD_ITEM_DISK::setMaster_ID(unsigned int value)
+{
+    master_ID = value;
+}
+
+file_type BFD_ITEM_DISK::getF_type() const
+{
+    return f_type;
+}
+
+void BFD_ITEM_DISK::setF_type(const file_type &value)
+{
+    f_type = value;
+}
+
+unsigned long long BFD_ITEM_DISK::getF_size() const
+{
+    return f_size;
+}
+
+void BFD_ITEM_DISK::setF_size(unsigned long long value)
+{
+    f_size = value;
+}
+
+unsigned int BFD_ITEM_DISK::getF_addr() const
+{
+    return f_addr;
+}
+
+void BFD_ITEM_DISK::setF_addr(unsigned int value)
+{
+    f_addr = value;
+}
+
+unsigned int BFD_ITEM_DISK::getF_link_num() const
+{
+    return f_link_num;
+}
+
+void BFD_ITEM_DISK::setF_link_num(unsigned int value)
+{
+    f_link_num = value;
+}
+
+time_t BFD_ITEM_DISK::getF_creat_time() const
+{
+    return f_creat_time;
+}
+
+void BFD_ITEM_DISK::setF_creat_time(const time_t &value)
+{
+    f_creat_time = value;
+}
+
+time_t BFD_ITEM_DISK::getF_change_time() const
+{
+    return f_change_time;
+}
+
+void BFD_ITEM_DISK::setF_change_time(const time_t &value)
+{
+    f_change_time = value;
+}
+
+unsigned int BFD_ITEM_DISK::getDinode_ID() const
+{
+    return dinode_ID;
+}
+
+void BFD_ITEM_DISK::setDinode_ID(unsigned int value)
+{
+    dinode_ID = value;
+}
+
+vector<char> BFD_ITEM_DISK::getAuth() const
+{
+    return auth;
+}
+
+void BFD_ITEM_DISK::setAuth(const vector<char> &value)
+{
+    auth = value;
+}
--- minode.h
+++ minode.h
@@ -9,21 +9,47 @@ using namespace std;
 
 class BFD_ITEM_MEMORY: public BFD_ITEM_DISK
 {
+public:
+    // D-inode向M-inode转化
+    BFD_ITEM_MEMORY(BFD_ITEM_DISK pre_dinode,
+                    unsigned int inode_ID,
+                    bool f_lock,                    // 文件锁定状态（true为锁定）
+                    unsigned int f_access_num,      // 文件访问计数
+                    BFD_ITEM_MEMORY* node1,
+                    BFD_ITEM_MEMORY* node2);
+
+    unsigned int getInode_ID() const;
+    void setInode_ID(unsigned int value);
+
+    bool getF_lock() const;
+    void setF_lock(bool value);
+
+    unsigned int getF_access_num() const;
+    void setF_access_num(unsigned int value);
+
+    BFD_ITEM_MEMORY *getNode1() const;
+    void setNode1(BFD_ITEM_MEMORY *value);
+
+    BFD_ITEM_MEMORY *getNode2() const;
+    void setNode2(BFD_ITEM_MEMORY *value);
+
 private:
-    unsigned int inode_ID;
+    unsigned int inode_ID;          // 内存索引结点
     bool f_lock;                    // 文件锁定状态（true为锁定）
     unsigned int f_access_num;      // 文件访问计数
-    BFD_ITEM_MEMORY* node1;
-    BFD_ITEM_MEMORY* node2;
+    BFD_ITEM_MEMORY* node1;         // 指向空闲链表的指针
+    BFD_ITEM_MEMORY* node2;         // 指向散列队列的指针
 };
 
 // BFD_MEMORY
 class BFD_MEMORY
 {
-private:
-    vector<BFD_ITEM_MEMORY> BFD_MEMORY_list;
 public:
     BFD_MEMORY();
+    vector<BFD_ITEM_MEMORY> getBFD_MEMORY_list() const;
+    void setBFD_MEMORY_list(const vector<BFD_ITEM_MEMORY> &value);
+private:
+    vector<BFD_ITEM_MEMORY> BFD_MEMORY_list;
 };
 
 #endif // INODE_H
