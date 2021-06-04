35eb12356e855aa311fc0fb5e35b68097e5e992a
计算机1801-包家华-20184378 <52906012+Samasara1@users.noreply.github.com>
2021-06-04 10:22:14 +0800

    修复删除bug

--- main.cpp
+++ main.cpp
@@ -16,14 +16,30 @@ int main(int argc, char *argv[])
 //    impl.Init();
 //    qDebug()<<"start2";
 //    qDebug()<<impl.get_all_disk_free_block().size();
-    DISK impl = DISK();
-    impl.Init();
-    impl.createNewFile("/1.txt", 0);
-    impl.saveFile(impl.findFile("/1.txt"), "hello!");
+//    for(auto i : impl.get_all_disk_free_block())
+//    {
+//        qDebug() << i;
+//    }
+//    DISK impl=DISK();
+//    impl.createNewFile("/1.txt",0);
 //    QString temp  = "1/2/3/";
 //    QStringList tem = temp.split("/", QString::SkipEmptyParts);
 //    for(auto i : tem)
 //    {
 //        qDebug() << i << "gah";
 //    }
+    DISK impl = DISK();
+    impl.Init();
+    impl.createNewDirectory("/dir1", 0);
+    impl.createNewDirectory("/dir1/dir2", 0);
+    impl.createNewFile("/dir1/dir2/1.txt", 0);
+    impl.saveFile(impl.findFile("/dir1/dir2/1.txt"), "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890");
+    qDebug() << impl.readFile("/dir1/dir2/1.txt");
+    impl.EditFile("/dir1/dir2/1.txt", "hello?");
+    qDebug() << "after:" << "   " << impl.readFile("/dir1/dir2/1.txt") << "   " << "   ";
+    impl.delFile("/dir1/dir2/1.txt");
+    //impl.EditFile("/dir1/dir2/1.txt", "hello222222?");
+    //qDebug() << "after:" << "   " << impl.readFile("/dir1/dir2/1.txt") << "   " << "   ";
+    qDebug() << impl.getFileList("/dir1/dir2/");
+
 }
